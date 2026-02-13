867f70363e1cf4faa623ab867b4905fff9ac8dab
Kirtan Patel <imkp004@gmail.com>
2026-02-13 12:00:27 -0500

    Add search option

--- main.py
+++ main.py
@@ -2,50 +2,88 @@ from tkinter import *
 from tkinter import messagebox
 from random import *
 import pyperclip
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 def generate_password():
-    
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-    password_list = [choice(letters) for i in range(randint(8, 10))]
-    password_list += [choice(symbols) for i in range(randint(2, 4))]
-    password_list += [choice(numbers) for i in range(randint(2, 4))]
+	letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+	numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+	symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-    # from random module
-    shuffle(password_list) 
+	password_list = [choice(letters) for i in range(randint(8, 10))]
+	password_list += [choice(symbols) for i in range(randint(2, 4))]
+	password_list += [choice(numbers) for i in range(randint(2, 4))]
 
-    password = "".join(password_list)
+	# from random module
+	shuffle(password_list)
+
+	password = "".join(password_list)
    
-    password_entry.delete(0, END)
-    password_entry.insert(0, password)
-    
-    pyperclip.copy(password)
-    
+	password_entry.delete(0, END)
+	password_entry.insert(0, password)
+
+	pyperclip.copy(password)
+
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 
 def save_data():
-    web = website_entry.get()
-    email = email_entry.get()
-    password = password_entry.get()
-    
-    if len(web) == 0 or len(email) == 0 or len(password) == 0:
-        messagebox.showwarning(title=None, message="One or more field is empty")
-    else:
-        check2 = messagebox.askokcancel(title=web, message=f"You entered\nEmail: {email}\nPassword: {password}\nWebsite: {web}\nDo you wish to save")
-        if check2 == True:
-            with open("data.txt", "a") as data:
-                data.write(f"{web} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
-    
-    website_entry.focus()
-        
+	website = website_entry.get().upper()
+	email = email_entry.get()
+	password = password_entry.get()
+
+	new_data = {
+		website: {
+			"email": email,
+			"password": password
+		}
+	}
+
+	if len(website) == 0 or len(password) == 0:
+		messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
+	else:
+		try:
+			with open("data.json", "r") as data_file:
+				data = json.load(data_file)
+				data.update(new_data)
+
+			with open("data.json", "w") as data_file:
+				json.dump(data, data_file, indent=4)
+
+				website_entry.delete(0, END)
+				password_entry.delete(0, END)
+
+		except:
+			with open("data.json", "w") as data_file:
+				json.dump(new_data, data_file, indent=4)
+
+				website_entry.delete(0, END)
+				password_entry.delete(0, END)
+
+	website_entry.focus()
+
+
+
+
+
+
+def search_website():
+	website = website_entry.get().upper()
+	web = website_entry.get()
+	try:
+		with open("data.json", "r") as data_file:
+			data = json.load(data_file)
+
+		try:
+			messagebox.showinfo(title=website, message=f"You already have this website information saved\nWebsite: {web}\nEmail: {data[website]["email"]}\nPassword: {data[website]["password"]}")
+		except KeyError:
+			messagebox.showinfo(title=website, message=f"{website} not found")
+
+	except:
+		messagebox.showinfo(title=None, message=f"You don't have anything saved")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -67,10 +105,12 @@ canvas.grid(column=1, row=0)
 website = Label(text="Website:")
 website.grid(column=0, row=1)
 
-website_entry = Entry(width=35)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry = Entry(width=20)
+website_entry.grid(column=1, row=1)
 website_entry.focus()
 
+Search = Button(text="Search", width=10, command=search_website)
+Search.grid(column=2, row=1)
 
 
 email = Label(text="Email/Username:")
